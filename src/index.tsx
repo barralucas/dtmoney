42f1b6d18f9d02f55b6ebf73aa0eb8f33d0f3e91
Lucas <lucastbarra@hotmail.com>
2021-06-10 12:16:12 -0300

    listando valores e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,30 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
@@ -17,7 +41,7 @@ createServer({
     })
 
 
-    this.post('/transactions', (schema, request) => { 
+    this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
       return schema.create('transaction', data)
